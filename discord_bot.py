86b2a510c988e8ef93bbb0e962061b2c4a8e4799
inforussle <123Jimmyn@gmail.com>
2022-04-13 01:00:09 -0400

    Modified to reflect changes to WorksheetConnection.py

--- discord_bot.py
+++ discord_bot.py
@@ -2,19 +2,34 @@
 import os
 from discord.ext import commands
 from dotenv import load_dotenv
+import WorksheetConnection as WSConnection
 
 # loads bot token from local .env file
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GSHEET_CRED = os.getenv('GOOGLE_SHEET_CREDENTIAL_FILE_PATH')
+active_file = active_sheet = None
 
-# initialized bot to respond to '!' commands
+# initialize bot to respond to '!' commands
 bot = commands.Bot(command_prefix='!')
 
 
 # prints connection confirmation to console
 @bot.event
 async def on_ready():
-    print("connected")
+    print("Connected to Discord.")
+
+
+@bot.command(name='set_sheet', help='Selects a Google Sheet as the target for commands.')
+async def select_google_sheet(ctx, workbook_name, sheet_name=None):
+    try:
+        global active_file
+        active_file = WSConnection.WorksheetConnection(GSHEET_CRED)
+    except WSConnection.AuthenticationFailed:
+        await ctx.send('Failed to authenticate with Google Sheets.  Check that your key file is correctly defined.')
+    else:
+        global active_sheet
+        active_sheet = active_file.open_sheet(workbook_name, sheet_name)
 
 
 # parses and logs participation, can only be invoked by Mysterium members
@@ -23,9 +38,9 @@ async def on_ready():
 @commands.has_role('Mysterium')
 async def log_participation(ctx):
     args = str(ctx.message.content).splitlines()[1:]
-    parsedInput = parse_participation(args)
+    parsed_input = parse_participation(args)
     message = ''
-    for line in parsedInput:
+    for line in parsed_input:
         message += f'{line[0]} gains {line[1]} point(s)\n'
     await ctx.send(message)
 
