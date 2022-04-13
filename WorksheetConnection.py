7c5e46ffdd0ec9bd8dc4865bde9c6ec269c2bc42
inforussle <123Jimmyn@gmail.com>
2022-04-13 00:58:59 -0400

    Generalized class initialization to be more flexible.  Added some catches for errors related to authentication with Google Sheets.

--- WorksheetConnection.py
+++ WorksheetConnection.py
@@ -2,36 +2,49 @@ from google.oauth2.service_account import Credentials
 import gspread
 
 
-# expects worksheet with headers
+class WorksheetConnectionException(Exception):
+    pass
+
+
+class AuthenticationFailed(WorksheetConnectionException):
+    pass
+
+
 class WorksheetConnection:
-    def __init__(self, file_name, sheet_name=0):
-        self.file_name = file_name
-        self.sheet_name = sheet_name
-        # authenticate and connect with Google api
+    def __init__(self, credential_file_path):
+        self.file_name = self.sheet_name = None
+        self.active_file = self.active_sheet = None
+
         scopes = [
             'https://www.googleapis.com/auth/spreadsheets',
             'https://www.googleapis.com/auth/drive']
 
         credentials = Credentials.from_service_account_file(
-            'participationbot-6278614d1e9c.json',
+            credential_file_path,
             scopes=scopes)
 
-        gc = gspread.authorize(credentials)
-        print('Authorized successfully.')
+        try:
+            self.gc = gspread.authorize(credentials)
+        except AuthenticationFailed as e:
+            print(f'Authentication failed: {e}')
+            raise AuthenticationFailed
+        else:
+            print('Authenticated successfully.')
 
-        self.open_file = gc.open(file_name)
-        self.active_sheet = self.open_file.get_worksheet(0)
+    def open_sheet(self, workbook, sheet):
+        self.file_name = workbook
+        self.sheet_name = sheet
+
+        self.active_file = self.gc.open(workbook)
+        try:
+            self.active_sheet = self.active_file.worksheet(sheet)
+        except TypeError:
+            self.active_sheet = self.active_file.sheet1
+
         print(f'{self.file_name} is open for editing.')
 
-        self.column_headings = self.active_sheet.row_values(1)
-        print(f'Available columns are: {self.column_headings}')
-
     def get_cell_value(self, row, column):
-        column = self.column_headings.index(column) + 1
-        row = row + 1
         return self.active_sheet.cell(row, column).value
 
     def update_cell_value(self, row, column, value):
-        column = self.column_headings.index(column) + 1
-        row = row + 1
         self.active_sheet.update_cell(row, column, value)
