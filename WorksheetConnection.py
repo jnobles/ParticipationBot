338fdb187b95f312403e4e21e05e7fb8aa920ed5
inforussle <123Jimmyn@gmail.com>
2022-05-06 20:40:23 -0400

    Fixed WorksheetConnection.py and Participation.py not properly opening a new file

--- WorksheetConnection.py
+++ WorksheetConnection.py
@@ -2,17 +2,11 @@ from google.oauth2.service_account import Credentials
 import gspread
 
 
-class WorksheetConnectionException(Exception):
-    pass
-
-
-class AuthenticationFailed(WorksheetConnectionException):
-    pass
-
-
 class WorksheetConnection:
     def __init__(self, credential_file_path):
         self.active_file = self.active_sheet = None
+        self.active_file_name = self.active_sheet_name = None
+        self.sheet_values = None
 
         scopes = [
             'https://www.googleapis.com/auth/spreadsheets',
@@ -24,8 +18,8 @@ class WorksheetConnection:
 
         try:
             self.gc = gspread.authorize(credentials)
-        except AuthenticationFailed as e:
-            print(f'Authentication failed: {e}')
+        except gspread.exceptions.GSpreadException:
+            print(f'Authentication with Google failed.  Check that your key file is correctly defined.')
         else:
             print('Authenticated with Google successfully.')
 
@@ -33,25 +27,37 @@ class WorksheetConnection:
         message = ''
         try:
             self.active_file = self.gc.open(file)
+            self.active_file_name = self.active_file.title
         except gspread.exceptions.SpreadsheetNotFound:
             message = f'{file} not found.'
         else:
             if sheet is None:
                 self.active_sheet = self.active_file.sheet1
-                message = f'{self.active_file.title} > {self.active_sheet.title} ' \
+                self.active_sheet_name = self.active_sheet.title
+                self.sheet_values = self.active_sheet.get_all_values()
+                message = f'{self.active_file_name} > {self.active_sheet_name} ' \
                           f'successfully opened for editing.'
             else:
                 try:
                     self.active_sheet = self.active_file.worksheet(sheet)
-                except gspread.exceptions.WorksheetNotFound:
-                    message = f'{sheet} not found in {self.active_file.title}'
-                else:
-                    message = f'{self.active_file.title} > {self.active_sheet.title} ' \
+                    self.active_sheet_name = self.active_sheet.title
+                    self.sheet_values = self.active_sheet.get_all_values()
+                    message = f'{self.active_file_name} > {self.active_sheet_name} ' \
                               f'successfully opened for editing.'
+                except gspread.exceptions.WorksheetNotFound:
+                    message = f'{sheet} not found in {self.active_file_name}'
         return message
+
+    def push_sheet_changes(self):
+        self.active_sheet.update(self.sheet_values)
 
     def get_cell_value(self, row, column):
         return self.active_sheet.cell(row, column).value
 
     def update_cell_value(self, row, column, value):
         self.active_sheet.update_cell(row, column, value)
+
+    def get_player_row_index(self, player_name, column_header):
+        # todo: remove these magic numbers
+
+        pass
--- cogs/Participation.py
+++ cogs/Participation.py
@@ -1,4 +1,5 @@
 import discord
+import gspread.exceptions
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
@@ -6,7 +7,7 @@ import WorksheetConnection as WSConnection
 
 load_dotenv()
 GSHEET_CRED = os.getenv('GOOGLE_SHEET_CREDENTIAL_FILE_PATH')
-active_file = active_sheet = None
+file = None
 
 
 class ParticipationCog(commands.Cog):
@@ -15,16 +16,15 @@ class ParticipationCog(commands.Cog):
 
     @commands.command(name='set_sheet',
                       description='Selects a Google Sheet as the target for commands.')
-    async def select_google_sheet(self, ctx, workbook_name, sheet_name=None):
+    async def select_sheet(self, ctx, workbook_name, sheet_name=None):
         try:
-            global active_file
-            active_file = WSConnection.WorksheetConnection(GSHEET_CRED)
-        except WSConnection.AuthenticationFailed:
+            global file
+            file = WSConnection.WorksheetConnection(GSHEET_CRED)
+        except gspread.exceptions.GSpreadException:
             await ctx.send('Failed to authenticate with Google Sheets.  Check that your key file is correctly defined.')
         else:
-            global active_sheet
-            active_sheet = active_file.open_sheet(workbook_name, sheet_name)
-            print(f'{active_sheet.file_name} is open for editing.')
+            await ctx.send(f'{file.open_sheet(workbook_name, sheet_name)}')
+            print(file.active_sheet_name)
 
     @commands.group(invoke_without_command=True)
     async def log(self, ctx):
@@ -40,25 +40,51 @@ class ParticipationCog(commands.Cog):
 
     @log.command(name='bonus',
                  description='Awards bonus points.')
-    async def bonus(self, ctx, *, args: str):
-        # todo: write parsing function
-        await ctx.send(f'{args}')
-
-    @log.command(name='poobadoo',
-                 description='Awards poobadoo points.')
-    async def poobadoo_points(self, ctx, *, args: str):
+    async def bonus(self, ctx, *, args):
         args = args.splitlines()
         parsed = []
         for line in args:
             temp = ['name', 1]
-            input_string = args.split()
+            input_string = line.split()
             temp[1] = input_string[-1]
-            temp[0] = input_string[:-1]
+            temp[0] = ''.join(input_string[:-1])
             parsed.append(temp)
+
+        message = ''
+        try:
+            # todo: attempt to write to spreadsheet
+            message = 'Successfully added:\n```\n'
+            message += ''.join([f'{item[0]} gains {item[1]} bonus point(s).\n' for item in parsed])
+            message += '```'
+        except Exception as e:
+            message = f'Error parsing: {e}'
+        await ctx.send(message)
+
+    @log.command(name='poobadoo',
+                 description='Awards poobadoo points.')
+    async def poobadoo_points(self, ctx, *, args):
+        args = args.splitlines()
+        parsed = []
+        for line in args:
+            temp = ['name', 1]
+            input_string = line.split()
+            temp[1] = input_string[-1]
+            temp[0] = ''.join(input_string[:-1])
+            parsed.append(temp)
+
+        message = ''
+        try:
+            # todo: attempt to write to spreadsheet
+            message = '```\n'
+            message += ''.join([f'{item[0]} gains {item[1]} poobadoo point(s).\n' for item in parsed])
+            message += '```'
+        except Exception as e:
+            message = f'Error parsing: {e}'
+        await ctx.send(message)
 
     @log.command(name='event',
                  description='Awards event participation.')
-    async def event_participation(self, ctx, *, args: str):
+    async def event_participation(self, ctx, *, args):
         args = args.splitlines()
         parsed = []
         for line in args:
@@ -66,18 +92,24 @@ class ParticipationCog(commands.Cog):
             input_string = line.split()
             if len(input_string) == 1:
                 temp[0] = ''.join(input_string)
-                break
-            if 'leader' in input_string.tolower():
-                # todo: this check will not work correctly if someone has a name containing a space and any part of
-                #  their name that isn't the first word contains the word 'leader'
+                parsed.append(temp)
+                continue
+            if '--leader' in input_string:
                 temp[1] = True
             if input_string[-1].isnumeric():
                 temp[2] = input_string[-1]
             temp[0] = ''.join(input_string[:-1])
             parsed.append(temp)
-        return parsed
 
-    # todo: rewrite call to WorksheetConnection.py for writing to sheet; parsing of bot commands has changed
+        message = '```\n'
+        try:
+            for item in parsed:
+                # todo: attempt to write to spreadsheet
+                message += ''.join(f'{item[0]}{"(Leader)" if item[1] else ""} gains {item[2]} event point(s).\n')
+            message += '```'
+        except Exception as e:
+            message = f'Error parsing: {e}'
+        await ctx.send(message)
 
 
 def setup(bot):
